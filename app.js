9065ad7c48c2226ca53ea2883ec44c4a514930c4
Spengs <Jotto465@gmail.com>
2016-08-02 23:10:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,45 @@
+$(document).ready(function(){
+var rightAnswer = 0
+// var colors = ["red", "yellow", "blue", "green"];
+$('body').append('<div class="bluebox"></div>');
+$('body').append('<div class="redbox"></div>');
+$('body').append('<div class="greenbox"></div>');
+$('body').append('<div class="yellowbox"></div>');
+console.log(rightAnswer);
+
+
+    function randomNumber(min, max){
+      rightAnswer = Math.floor(Math.random() * (1 + max - min) + min);
+    }
+
+    randomNumber(1,4);
+    console.log(rightAnswer);
+      if(rightAnswer == 1) {
+        alert("Click Blue!");
+        $(".bluebox").on("click", function(){
+          alert("Great Job!");
+        })
+      }else if(rightAnswer == 2){
+        alert("Click Red!");
+        $(".redbox").on("click", function(){
+          alert("Great Job!");
+        })
+      }else if(rightAnswer == 3){
+        alert("Click Green!");
+        $(".greenbox").on("click", function(){
+          alert("Great Job!");
+        })
+      }else if(rightAnswer == 4){
+        alert("Click Yellow!");
+        $(".yellowbox").on("click", function(){
+          alert("Great Job!");
+        })
+      }
+      $("body").on("click", function(){
+        alert("Try Again!");
+      })
+
+console.log(rightAnswer);
+
+
+});
